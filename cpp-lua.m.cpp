f750c1e923bdb4795ab974c0616b20c16c224c68
Uysal Koyuncu <zhuyongzhe3141592@gmail.com>
2025-02-22 17:10:06 -0500

    switching to CMake

--- cpp-lua.m.cpp
+++ cpp-lua.m.cpp
@@ -7,7 +7,7 @@ extern "C" {
 int main(int argc, char *argv[]) {
   lua_State *L = luaL_newstate();
   luaL_openlibs(L);
-  luaL_dofile(L, "script.lua");
+  luaL_dofile(L, "../script.lua");
   lua_close(L);
   return 0;
 }
